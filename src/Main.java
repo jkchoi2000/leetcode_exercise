9fd1fab34f4a79e8e336f2ad89086d2008597d98
jkchoi2000 <jkchoi2000@gmail.com>
2019-11-20 18:36:41 +0900

    RomanToInteger : 로마 문자열을 입력하면 숫자 값으로 반환

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import mine.jkchoi.leetcode.ContainerWithMostWater;
 import mine.jkchoi.leetcode.IntegerToRoman;
+import mine.jkchoi.leetcode.RomanToInteger;
 import mine.jkchoi.leetcode.StringToInteger;
 
 public class Main {
@@ -18,11 +19,18 @@ public class Main {
 //        ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
 //        System.out.println(containerWithMostWater.maxArea(new int[] {1,8,6,2,5,4,8,3,7}));
 
-        IntegerToRoman integerToRoman = new IntegerToRoman();
-        System.out.println(integerToRoman.intToRoman(3));
-        System.out.println(integerToRoman.intToRoman(4));
-        System.out.println(integerToRoman.intToRoman(9));
-        System.out.println(integerToRoman.intToRoman(58));
-        System.out.println(integerToRoman.intToRoman(1994));
+//        IntegerToRoman integerToRoman = new IntegerToRoman();
+//        System.out.println(integerToRoman.intToRoman(3));
+//        System.out.println(integerToRoman.intToRoman(4));
+//        System.out.println(integerToRoman.intToRoman(9));
+//        System.out.println(integerToRoman.intToRoman(58));
+//        System.out.println(integerToRoman.intToRoman(1994));
+
+        RomanToInteger romanToInteger = new RomanToInteger();
+        //System.out.println(romanToInteger.romanToInt("III"));
+        System.out.println(romanToInteger.romanToInt("IV"));
+        System.out.println(romanToInteger.romanToInt("IX"));
+        System.out.println(romanToInteger.romanToInt("LVIII"));
+        System.out.println(romanToInteger.romanToInt("MCMXCIV"));
     }
 }
--- src/mine/jkchoi/leetcode/RomanToInteger.java
+++ src/mine/jkchoi/leetcode/RomanToInteger.java
@@ -1,8 +1,11 @@
 package mine.jkchoi.leetcode;
 
 public class RomanToInteger {
+    //Result
+    //Runtime: 5 ms, faster than 59.28% of Java online submissions for Roman to Integer.
+    //Memory Usage: 36.9 MB, less than 100.00% of Java online submissions for Roman to Integer.
     public int romanToInt(String s) {
-        if(s == null || s.isEmpty()) {
+        if (s == null || s.isEmpty()) {
             return 0;
         }
 
@@ -12,5 +15,34 @@ public class RomanToInteger {
         int[] otherValues = new int[]{1, 4, 9, 40, 90, 400, 900};
         String[] otherSymbols = new String[]{"", "IV", "IX", "XL", "XC", "CD", "CM"};
 
+        int result = 0;
+        int searchIndex = symbols.length - 1;
+        int strLength = s.length();
+
+        for (int i = 0; i < strLength;) {
+            String indexStr = String.valueOf(s.charAt(i));
+
+            while (searchIndex >= 0) {
+                if (indexStr.equals(symbols[searchIndex])) {
+                    result += values[searchIndex];
+                    i++;
+                    break;
+                }
+
+                if(i < (strLength - 1)) {
+                    String twoStr = s.substring(i, i + 2);
+                    if (twoStr.equals(otherSymbols[searchIndex])) {
+                        result += otherValues[searchIndex];
+                        i += 2;
+                        break;
+                    }
+                }
+
+                searchIndex--;
+
+            }
+        }
+
+        return result;
     }
 }
